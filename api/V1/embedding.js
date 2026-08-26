a8b9c98fc079568f2b17309cf4b41de497e8b1a7
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-08-26 15:34:00 +0100

    Create embedding.js

---
+++ api/V1/embedding.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+const options = {
+  method: 'POST',
+  url: 'https://qubu.ai/api/v1/embeddings',
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: 'Bearer d369192cb0dc2c4fbba6c17d153fbaeb5056a9c1\n'
+  },
+  data: {
+    input: 'Привет, мир',
+    model: 'text-embedding-3-small',
+    encoding_format: 'float'
+  }
+}
+
+try {
+  const { data } = await axios.request(options)
+  console.log(data)
+} catch (error) {
+  console.error(error)
+}
